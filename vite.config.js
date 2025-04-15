3cfc5e144845d749b3d6359cea163a84c3e37572
AffanPathan02 <pathan.affan02@gmail.com>
2025-04-15 18:22:57 +0530

    added bun cmds scripts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AffanPathan02.github.io/",
+  base:
+    process.env.NODE_ENV === "production" &&
+    process.env.DEPLOY_TARGET === "gh-pages"
+      ? "/AffanPathan02.github.io/"
+      : "/",
 });
